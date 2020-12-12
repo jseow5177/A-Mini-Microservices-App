562260c1e69f853bc5e9117fbf3e219e78f15d93
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-12 14:18:47 +0800

    Add controller for comments

---
+++ comments/comment.controller.js
@@ -0,0 +1,29 @@
+const { randomBytes } = require('crypto')
+const commentsByPostId = require('./index')
+
+const commentControllers = {
+
+    addNewComment: function (req, res) {
+        const commentId = randomBytes(4).toString('hex')
+        const { content } = req.body    
+
+        // Check if the post has existing comments
+        const comments = commentsByPostId[req.params.id] || []
+
+        comments.push({
+            id: commentId,
+            content
+        })
+
+        commentsByPostId[req.params.id] = comments
+
+        return res.status(201).send(comments)
+    },
+
+    retrieveComments: function (req, res) {
+        return res.send(commentsByPostId[req.params.id] || [])
+    }
+
+}
+
+module.exports = commentControllers
