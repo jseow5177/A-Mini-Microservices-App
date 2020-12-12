9f422157e0d61540cd5cce41ef511c126039f896
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-12 16:23:24 +0800

    Add CommentCreate component

---
+++ client/src/comments/CommentCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function CommentCreate ({ postId }) {
+  const [content, setContent] = useState('')
+
+  const handleChangeContent = (e) => {
+    setContent(e.target.value)
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    })
+
+    setContent('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input value={content} onChange={handleChangeContent} className="form-control"/>
+        </div>
+        <button className="btn btn-primary mt-2">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default CommentCreate
