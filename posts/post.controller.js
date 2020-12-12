4588fecb5296a2bc71877143a473c619c9370a9c
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-12 14:19:47 +0800

    Add controller for posts

---
+++ posts/post.controller.js
@@ -0,0 +1,24 @@
+const { randomBytes } = require('crypto')
+const posts = require('./index')
+
+const postControllers = {
+
+    addNewPost: function (req, res) {
+        const postId = randomBytes(4).toString('hex') // Create random hexadecimal string
+        const { title } = req.body
+
+        // Save new post created
+        posts[postId] = {
+            postId, title
+        }
+
+        return res.status(201).send(posts[postId])
+    },
+
+    retrievePosts: function (req, res) {
+        return res.status(200).send(posts)
+    }
+
+}
+
+module.exports = postControllers
