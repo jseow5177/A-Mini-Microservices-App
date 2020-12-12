4a9dce75db5581403e97a791f83a1ed36ae94c04
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-12 14:18:58 +0800

    Add routes for comments

---
+++ comments/comment.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const commentControllers = require('./comment.controller')
+
+const router = express.Router()
+
+router.route('/posts/:id/comments')
+    .get(commentControllers.retrieveComments)
+    .post(commentControllers.addNewComment)
+
+module.exports = router
