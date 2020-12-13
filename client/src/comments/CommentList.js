4348d585dd62c724a5cd8426316179faa130bd3d
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-13 13:47:03 +0800

    Remove the need to fetch comments

--- client/src/comments/CommentList.js
+++ client/src/comments/CommentList.js
@@ -1,19 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-function CommentList ({ postId }) {
-  const [comments, setComments] = useState([])
-
-  const fetchComments = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-    setComments(res.data)
-  }
-
-  useEffect(() => {
-    fetchComments()
-  }, [])
-
+function CommentList ({ comments }) {
+  
   const renderedComments = comments.map(comment => {
     return (
       <li key={comment.commentId}>
