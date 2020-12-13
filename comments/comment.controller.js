22ed566e8f49c4b4dfd2853145d32907b49f56a1
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-13 13:47:44 +0800

    Emit event to event bus on comment creation

--- comments/comment.controller.js
+++ comments/comment.controller.js
@@ -1,9 +1,10 @@
+const axios = require('axios')
 const { randomBytes } = require('crypto')
 const commentsByPostId = require('./index')
 
 const commentControllers = {
 
-  addNewComment: function (req, res) {
+  addNewComment: async function (req, res) {
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body    
 
@@ -17,11 +18,26 @@ const commentControllers = {
 
     commentsByPostId[req.params.id] = comments
 
+    // Emit event to event bus
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        commentId,
+        content,
+        postId: req.params.id
+      }
+    })
+
     return res.status(201).send(comments)
   },
 
   retrieveComments: function (req, res) {
     return res.send(commentsByPostId[req.params.id] || [])
+  },
+
+  receiveEvent: function (req, res) {
+    console.log(req.body)
+    return res.status(200).send({ status: 'OK' })
   }
 
 }
