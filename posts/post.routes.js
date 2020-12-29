f4edd14ae491c60244ef3bba2a2da502f9891bea
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-29 22:24:21 +0800

    Update API url

--- posts/post.routes.js
+++ posts/post.routes.js
@@ -3,8 +3,11 @@ const postControllers = require('./post.controller')
 
 const router = express.Router()
 
+// This route is unused. posts retrieval is done though the query service
 router.route('/posts')
   .get(postControllers.retrievePosts)
+
+router.route('/posts/create')
   .post(postControllers.addNewPost)
 
 router.route('/events')
